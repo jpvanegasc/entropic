af6fd6ddc8e8b9d92637a28220078460f5c3f312
Juan Vanegas <jpvanegasc@gmail.com>
2023-11-13 00:22:51 -0500

    :sparkles: Basic results behavior

--- entropic/results/results.py
+++ entropic/results/results.py
@@ -1,2 +1,12 @@
+from typing import Iterable
+
+from entropic.sources import Case
+from entropic.db import default_database
+
+
 class Results:
-    ...
+    database = default_database()
+
+    @property
+    def all(self) -> Iterable[Case]:
+        return self.database.find()
