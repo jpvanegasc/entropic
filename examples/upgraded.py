badbe6d6f7d03c0dcc26c62d01c1be702a7149f1
Juan Vanegas <jpvanegasc@gmail.com>
2023-11-29 22:28:25 -0500

    :sparkles: Add upgraded example

--- examples/upgraded.py
+++ examples/upgraded.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from entropic.sources import Sample
+from entropic.sources import Sample, Iteration
 from entropic.process import Pipeline
 from entropic import results
 
@@ -8,21 +8,31 @@ class KinematicSample(Sample):
     speed: float = 0
 
 
+class KinematicExperiment(Iteration):
+    average_speed: float = 0
+    sample = KinematicSample
+
+
 class Process(Pipeline):
-    source_paths = ["tests/mocks"]
+    source_paths = ["../tests/mocks"]
     extract_with = pd.read_csv
-    sample = KinematicSample  # this will set the sample in the iteration, most likely in the metaclass __new__
+    iteration = KinematicExperiment
 
     def transform(self, iteration):
+        average = 0
         for sample in iteration.samples:
             sample.speed = (sample.data.raw["x"] / sample.data.raw["t"]).mean()
+            average += sample.speed
+        iteration.average_speed = average
 
 
 p = Process()
 p.run()
 
+results.set_iteration(KinematicExperiment)
+
 if __name__ == "__main__":
     for iteration in results.all:
-        print(iteration.source_path)
+        print(iteration.average_speed)
         for sample in iteration.samples:
             print(sample.speed)
