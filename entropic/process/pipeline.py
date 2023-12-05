dae9073c64c3399a81c844137f8178278ee5b715
Juan Vanegas <jpvanegasc@gmail.com>
2023-12-05 17:26:34 -0500

    :truck: Move documentation to source code

--- entropic/process/pipeline.py
+++ entropic/process/pipeline.py
@@ -44,6 +44,19 @@ class PipelineMeta(type):
 
 
 class Pipeline(metaclass=PipelineMeta):
+    """
+    The Pipeline class is a base class designed to facilitate the creation of data processing pipelines. It enforces a structured approach to defining and executing data processing steps, such as extraction, transformation, and loading (ETL).
+
+    - **iteration**: `Iteration`
+      - Iteration to be used for processing. Defaults to `Iteration`
+
+    - **source_paths**: List[Union[Path, str]]
+      - A list of path-like objects or strings representing the source paths from which data will be extracted.
+
+    - **extract_with**: Callable
+      - A callable object or function responsible for extracting data from a given file path into samples for each iteration.
+    """
+
     iteration = Iteration
     source_paths: list[Union[Path, str]] = []
     extract_with: Callable
@@ -62,21 +75,35 @@ class Pipeline(metaclass=PipelineMeta):
                 raise TypeError(error_message) from ex
 
     def get_source_paths(self):
+        """Returns the list of source paths defined in the pipeline."""
         return self.source_paths
 
     def get_iteration(self):
+        """
+        Returns the iteration type associated with the pipeline.
+        """
         return self.iteration
 
     def get_iteration_by_path(self, source_path):
+        """
+        Wrapper over `Iteration.get_or_create(source_path=source_path)`
+        """
         return self.get_iteration().get_or_create(source_path=source_path)
 
     def get_sample(self):
+        """
+        Returns the sample associated with the iteration.
+        """
         return self.iteration.sample
 
     def get_files_from_path(self, path):
+        """
+        Retrieves a list of `Path` objects representing files in the specified path.
+        """
         return [Path(path, file) for file in os.listdir(path)]
 
     def extract(self, source_path):
+        """Extracts data from the specified file using the defined extract_with method and returns an Iteration object."""
         iteration = self.get_iteration_by_path(source_path)
         for file_path in self.get_files_from_path(source_path):
             data_source_data = self.extract_with(file_path)
@@ -87,13 +114,22 @@ class Pipeline(metaclass=PipelineMeta):
         return iteration
 
     def transform(self, iteration):
+        """
+        Transforms the given iteration and returns the transformed iteration.
+        """
         return iteration
 
     def load(self, iteration):
+        """
+        Loads the specified iteration by saving it.
+        """
         return iteration.save()
 
     @final
     def extract_all_iterations(self) -> list[IterationType]:
+        """
+        Extracts data from all specified source paths and returns a list of iterations.
+        """
         iterations: list[IterationType] = []
         for source_path in self.get_source_paths():
             iteration: IterationType = self.extract(source_path)
@@ -104,12 +140,18 @@ class Pipeline(metaclass=PipelineMeta):
     def transform_all_iterations(
         self, iterations: list[IterationType]
     ) -> list[IterationType]:
+        """
+        Transforms a list of iterations and returns the transformed list.
+        """
         for iteration in iterations:
             iteration = self.transform(iteration)
         return iterations
 
     @final
     def load_all_iterations(self, iterations: list[IterationType]) -> list:
+        """
+        Loads a list of iterations by saving them, and returns the list of results.
+        """
         saved = []
         for iteration in iterations:
             result = self.load(iteration)
@@ -118,6 +160,9 @@ class Pipeline(metaclass=PipelineMeta):
 
     @final
     def run(self):
+        """
+        Executes the entire pipeline, including extraction, transformation, and loading, and returns the final results.
+        """
         iterations = self.extract_all_iterations()
         iterations = self.transform_all_iterations(iterations)
         results = self.load_all_iterations(iterations)
--- entropic/sources/iteration.py
+++ entropic/sources/iteration.py
@@ -11,6 +11,22 @@ SampleType = TypeVar("SampleType")
 
 
 class Iteration(BaseModel, Generic[SampleType]):
+    """
+    The Iteration class is a generic class designed to represent iterations of data processing. It includes methods for serialization, validation, retrieval, and storage of data samples associated with a specific source path.
+
+    - **source_path**: Path
+        - Source path associated with the iteration. Will be used as primary key.
+
+    - **database**: ClassVar
+        - The database used for storage.
+
+    - **sample**: ClassVar
+        - The sample class associated with the iteration. Defaults to `Sample`.
+
+    - **samples**: List[SampleType]
+        - A list of samples associated with the iteration. Initialized as an empty list.
+    """
+
     database: ClassVar = default_database()
     sample: ClassVar = Sample
 
@@ -32,18 +48,27 @@ class Iteration(BaseModel, Generic[SampleType]):
 
     @classmethod
     def get_or_create(cls, **kwargs):
+        """
+        Retrieves an existing iteration from the database or creates a new one based on the provided keyword arguments.
+        """
         # TODO: this should be done automatically by the database
         if path := kwargs.get("source_path"):
             kwargs["source_path"] = str(path)
         return cls(**cls.database.get_or_create(**kwargs))
 
     def save(self):
+        """
+        Saves the iteration in the database using the serialized data.
+        """
         return self.database.upsert(
             self.model_dump(),
             key={"key": "source_path", "value": str(self.source_path)},
         )
 
     def upsert_sample(self, sample):
+        """
+        Updates or inserts a sample into the iteration's sample list.
+        """
         if sample in self.samples:
             index = self.samples.index(sample)
             self.samples[index] = sample
