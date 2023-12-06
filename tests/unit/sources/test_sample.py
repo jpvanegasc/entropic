4701b2fc3b430f37d3a88b191547d2d8e0d7103f
Juan Vanegas <jpvanegasc@gmail.com>
2023-12-06 18:42:52 -0500

    :white_check_mark: Get source fields

--- tests/unit/sources/test_sample.py
+++ tests/unit/sources/test_sample.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
-from entropic.sources import DefaultSample
+from entropic.sources import DefaultSample, BaseSample
+from entropic.sources.fields import DataSource
 
 
-def test_sample_data_source():
+def test_default_sample_data_source():
     data_field = {
         "file_path": "tests/mocks/kinematic1.csv",
         "raw": pd.read_csv("tests/mocks/kinematic1.csv"),
@@ -12,3 +13,17 @@ def test_sample_data_source():
     assert sample.data
     assert str(sample.data.file_path) == data_field["file_path"]
     assert not sample.data.raw.empty
+
+
+def test_get_source_fields(mock_data_frame):
+    class TestSample(BaseSample):
+        data_field_1: DataSource
+        data_field_2: DataSource
+        int_field: int
+
+    test = TestSample(
+        data_field_1=DataSource(file_path="path", raw=mock_data_frame),
+        data_field_2=DataSource(file_path="other/path", raw=mock_data_frame),
+        int_field=1,
+    )
+    assert test._get_data_source_fields() == ["data_field_1", "data_field_2"]
