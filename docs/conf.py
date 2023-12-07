d8df8854d47b1cdbe67327ff6f45e9404c55b629
Juan Vanegas <jpvanegasc@gmail.com>
2023-12-06 23:15:16 -0500

    :construction_worker: Allow build from docs

--- docs/conf.py
+++ docs/conf.py
@@ -5,11 +5,25 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+import pkg_resources
+
+sys.path.insert(0, os.path.abspath(".."))
 
 project = "entropic"
 copyright = "2023, Juan Pablo Vanegas"
 author = "Juan Pablo Vanegas"
-release = "0.2.0"
+
+try:
+    release = pkg_resources.get_distribution("entropic").version
+except pkg_resources.DistributionNotFound:
+    print("To build the documentation, The distribution information of entropic")
+    print("has to be available. Either install the package into your")
+    print('development environment or run "pip install -e ." to setup the')
+    print("metadata. A virtualenv is recommended!")
+    sys.exit(1)
+del pkg_resources
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
